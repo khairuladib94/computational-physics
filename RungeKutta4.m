543fdce73f38f8428b0c56dc5a37548850782014
Adib Yusof <adib.yusof@upm.edu.my>
2021-09-17 11:43:47 +0800

    Solutions stored as table

--- RungeKutta4.m
+++ RungeKutta4.m
@@ -3,7 +3,7 @@ Y0 = 1;
 YFunction = @(t, y) t*y;
 StepSize = 1/4;
 Interval = [0, 1];
-%% Calculation
+%% Approximation
 T = Interval(1) : StepSize : Interval(2);
 W(1) = Y0;
 for t = 1 : numel(T) - 1
@@ -13,4 +13,5 @@ for t = 1 : numel(T) - 1
     S4 = YFunction(T(t) + StepSize, W(t) + StepSize*S3);
     W(t + 1) = W(t) + (StepSize/6)*(S1 + 2*S2 + 2*S3 + S4);    
 end
+Solutions = table(T', W', 'VariableName', {'T', 'W'});
 
